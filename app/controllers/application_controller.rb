aa05394c2596f6deed3445b1d06771f18b44f47d
Jed Peek <jedpeek@yahoo.com>
2018-07-30 14:56:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,5 +30,7 @@ class ApplicationController < Sinatra::Base
 
   get '/recipes/new' do
     erb :'/new'
+    @recipe = Recipe.create(name:params[:name], ingredients:params[:ingredients], cook_time:params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
   end
 end
